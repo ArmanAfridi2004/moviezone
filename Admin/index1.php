7408c294c3dac377d93c380a3b90eaa9fd431b78
Arman Afridi <armanafridi86@gmail.com>
2022-07-30 23:11:18 +0600

    Added Admin folder

---
+++ Admin/index1.php
@@ -0,0 +1,50 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Online Library Management System</title>
+    <link rel = "stylesheet" type = "text/css" href = "style.css">
+</head>
+<body>
+    <div class = "wrapper">
+        <header>
+            <div class= "header_index"></div>
+            <br><br>
+                  <div><h1 style = " color:#FFEBCD; font-size:50px; text-align:left;"> &nbsp; &emsp; &nbsp; &emsp; &emsp; Online Library Management System</h1></div>
+            <br>
+            <nav>  
+                    <table> 
+                    <tr>
+                    <ul>
+                    <td><li style="list-style-type: none;"><a style = "color:white; text-decoration:none; font-size:21px;" href = "index.php"> &nbsp; &nbsp; &emsp; &nbsp; &emsp; &nbsp; &emsp; &nbsp; &emsp; &emsp; &nbsp; &emsp; &nbsp; &nbsp; Home</a></li></td>
+                    <td><li style="list-style-type: none;"><a style = "color:white; text-decoration:none; font-size:21px;" href = "book.php"> &nbsp; &emsp; Book</a></li></td>
+                    <td><li style="list-style-type: none;"><a style = "color:white; text-decoration:none; font-size:21px;" href = "login.php"> &nbsp; &emsp; Student Login</a></li></td>
+                    <td><li style="list-style-type: none;"><a style = "color:white; text-decoration:none; font-size:21px;" href = "register.php"> &nbsp; &emsp; Registration</a></li></td>
+                    <td><li style="list-style-type: none;"><a style = "color:white; text-decoration:none; font-size:21px;" href = "feedback.php"> &nbsp; &emsp; Feedback</a></li></td>
+                    </ul>
+                    </tr>
+                    </table>
+                    </nav>
+        </header>
+
+        <section>
+            <div class= "index_mid_section">
+                <br><br><br>
+                <div class = "box">
+                    <br><br><br>
+                    <h1 style = "text-align: center; font-size:35px; color:GAINSBORO;">Welcome To Library</h1>
+                    <h1 style = "text-align: center; font-size:24px; color:GAINSBORO;">OPENS AT 09:00am</h1>
+                    <h1 style = "text-align: center; font-size:24px; color:GAINSBORO;">CLOSES AT 03:00pm</h1>
+                </div>
+            </div>
+        </section>
+    
+</body>
+<footer>
+    <div class= "index_footer"></div>
+</footer>
+</div>
+</html>
